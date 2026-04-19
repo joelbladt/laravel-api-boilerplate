20da58314e678d39675b908f19d0240873ac2d7f
joelbladt <joel@bladt.de>
2026-04-19 22:29:38 +0200

    Handle unauthenticated API requests

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(function (Request $request) {
+            if ($request->is('api/*') || $request->expectsJson()) {
+                return null;
+            }
+
+            return url('/login');
+        });
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
